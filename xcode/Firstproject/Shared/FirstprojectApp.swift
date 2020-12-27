7e1819c6fcfe68a65f848a1acb3c1d9d335122b8
dukyoung.kim <dukyoung.kim@rakuten.com>
2020-12-27 23:42:02 +0900

    push 1st practice

---
+++ xcode/Firstproject/Shared/FirstprojectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstprojectApp.swift
+//  Shared
+//
+//  Created by Kim, Dukyoung on 2020/12/23.
+//
+
+import SwiftUI
+
+@main
+struct FirstprojectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
